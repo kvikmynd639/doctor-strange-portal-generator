e580b9be807b6d2ec89a0ef5fce0d59e89e4d7a1
Sumeja <sumeja.buljugija@gmail.com>
2024-05-14 09:44:55 +0200

    portal framer motion

--- src/components/Portal.js
+++ src/components/Portal.js
@@ -1,13 +1,17 @@
 import React from 'react';
+import { motion } from 'framer-motion';
 
 const Portal = ({ id, x, y, onRemove }) => {
   return (
-    <div
+    <motion.div
       className="w-20 h-20 bg-blue-500 rounded-full absolute"
       style={{ top: `${y}px`, left: `${x}px` }}
+      initial={{ scale: 0 }}
+      animate={{ scale: 1 }}
+      exit={{ scale: 0 }}
     >
       <button className="text-white" onClick={() => onRemove(id)}>X</button>
-    </div>
+    </motion.div>
   );
 };
 
